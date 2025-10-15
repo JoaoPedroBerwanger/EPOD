be0807c41ebd00623af0a0bdbbf6059053c70a38
JoaoPedroBerwanger <joaopedroberwanger12@gmail.com>
2025-10-14 23:14:17 -0300

    Termino Atividade

--- ArquivosCSV/src/ArquivoCSV/Main.java
+++ ArquivosCSV/src/ArquivoCSV/Main.java
@@ -1,57 +1,82 @@
 package ArquivoCSV;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Set;
 import java.util.Stack;
+import java.util.TreeSet;
 
 public class Main {
-
-	@SuppressWarnings("unchecked")
-	public static void exibeAcoes(List<Acao> acoes) {
-
-		Stack<String> pilhaAcoes = new Stack<>();
-		Queue<String> acoesDesfeitas = new LinkedList<>();
-
-		for (Acao acao : acoes) {
-
-			if (acao.getOperacao().equals("Feito"))
-			{
-				pilhaAcoes.push(acao.getAcao());
-			}
-			else if (acao.getOperacao().equals("Desfeito"))
-			{
-				if (!pilhaAcoes.isEmpty()) 
-				{
-					String ultima = pilhaAcoes.pop();
-					acoesDesfeitas.add(ultima);
-				}
-			}
-		}
-
-        System.out.println("Ações realizadas:");
-        for (String acao : pilhaAcoes) {
-            System.out.println(acao);
-        }
-        System.out.println();
-
-        System.out.println("Ações desfeitas:");
-        for (String acao : acoesDesfeitas) {
-            System.out.println(acao);
-        }
-        System.out.println();
-
-        System.out.println("Ações recentes:");
-        Stack<String> pilhaExcluir = (Stack<String>) pilhaAcoes.clone();
-        
-        while (!pilhaExcluir.isEmpty())
-            System.out.println(pilhaExcluir.pop());
-	}
 		
 	public static void main(String[] args) {
 	
-		List<Acao> acao = LeitorCSV.lerArquivo("src/inputs/editor.csv");		
-		exibeAcoes(acao);
-	
+		List<Participante> participantes = LeitorCSV.lerArquivo("src/inputs/eventos.csv");
+		
+		Set<Participante> eventoA = new TreeSet<>();
+		Set<Participante> eventoB = new TreeSet<>();
+		Set<Participante> eventoC = new TreeSet<>();
+
+		String caminho = "src/inputs/eventos.csv";
+		try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+            String linha;
+            
+            // Pular a primeira linha (cabeçalho)
+            br.readLine();
+            
+            // Lendo os registros
+            while ((linha = br.readLine()) != null) {
+                String[] campos = linha.split(",");
+                Integer numero = Integer.parseInt(campos[0]);
+                String nome = campos[1];
+                String evento = campos[2];
+                
+                Participante p = new Participante(numero, nome);
+                
+                switch (evento.toLowerCase()) {
+                	case "eventoa":
+                		eventoA.add(p);
+                		break;
+                	case "eventob":
+                		eventoB.add(p);
+                		break;
+                	case "eventoc":
+                		eventoC.add(p);
+                		break;
+				}
+            }
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+		Set<Participante> todosOrdenadosPorNome = new TreeSet<>(Comparator.comparing(Participante::getNome));
+        
+		todosOrdenadosPorNome.addAll(eventoA);
+        todosOrdenadosPorNome.addAll(eventoB);
+        todosOrdenadosPorNome.addAll(eventoC);
+
+        System.out.println("Participantes ordenados por nome:");
+        todosOrdenadosPorNome.forEach(System.out::println);
+        System.out.println();
+
+        Set<Participante> EventoAeEventoB = new TreeSet<>(eventoA);
+        EventoAeEventoB.retainAll(eventoB); 
+        
+        System.out.println("Participantes que participaram do evento A e também do evento B:");
+        EventoAeEventoB.forEach(System.out::println);
+        System.out.println();
+
+        Set<Participante> NaoForamC = new TreeSet<>(eventoA);
+        NaoForamC.addAll(eventoB);
+        NaoForamC.removeAll(eventoC); 
+        
+        System.out.println("Participantes que não participaram do evento C:");
+        NaoForamC.forEach(System.out::println);
+        
 	}
 }
--- ArquivosCSV/src/ArquivoCSV/Participante.java
+++ ArquivosCSV/src/ArquivoCSV/Participante.java
@@ -1,5 +1,7 @@
 package ArquivoCSV;
 
+import java.util.Objects;
+
 public class Participante implements Comparable<Participante> {
 	private Integer numero;
 	private String nome;
@@ -20,6 +22,29 @@ public class Participante implements Comparable<Participante> {
 
 	@Override
 	public int compareTo(Participante p) {
-		return 0;
+		return this.numero.compareTo(p.numero);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(numero, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Participante other = (Participante) obj; 
+		return Objects.equals(numero, other.getNumero()) && Objects.equals(nome, other.getNome());
+	}
+
+
+	@Override
+	public String toString() {
+		return nome + " (Registro: " + numero + ")";
 	}
 }
